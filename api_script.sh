b80509a2a01db3a40cdf809a809b3f8bec0811f5
Grzegorz <grzegorzjaceklorenc@gmail.com>
2023-02-25 00:30:00 +0100

    second

--- api_script.sh
+++ api_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Welcome to the True or False Game!"
+echo "Login message: "
+curl http://127.0.0.1:8000/login --silent --cookie-jar cookie.txt --user rihanna:785bdf267c5244
 
-curl http://127.0.0.1:8000/download/file.txt --silent --output ID_card.txt
-
-cat ID_card.txt
