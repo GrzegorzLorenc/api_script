842dcc7b3f42140f420e3fe2a7ef739327bb3aa3
Grzegorz <grzegorzjaceklorenc@gmail.com>
2023-02-25 10:45:45 +0100

    third

--- api_script.sh
+++ api_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Welcome to the True or False Game!"
-echo "Login message: "
+echo "Login message: " 
 curl http://127.0.0.1:8000/login --silent --cookie-jar cookie.txt --user rihanna:785bdf267c5244
+echo "Response: $(curl -s http://127.0.0.1:8000/game --cookie cookie.txt)"
 
